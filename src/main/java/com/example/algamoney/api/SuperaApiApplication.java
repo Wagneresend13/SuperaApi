0d6039493b0a7f12c2005358916761007c4fd2ea
WAGNER DA SILVA RESENDE JUNIOR <85854470+Wagneresend13@users.noreply.github.com>
2021-10-05 19:42:50 -0300

    commit 01

---
+++ src/main/java/com/example/algamoney/api/SuperaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.algamoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperaApiApplication.class, args);
+	}
+
+}
